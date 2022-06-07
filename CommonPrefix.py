8926e19d5c724117fa76d86399dc40e5d7c0a57f
sawantvidisha10 <sawantvidisha10@gmail.com>
2022-06-07 14:54:16 -0700

    updates

--- CommonPrefix.py
+++ CommonPrefix.py
@@ -1,17 +1,16 @@
-class CommonPrefix:
-    def longestCommonPrefix(self, strs: list[str]) -> str:
-        res = ""
+def longestCommonPrefix(strs: list[str]):
+    res = ""
 
-        if len(strs) == 0:
-            return res
-        if len(strs) == 1:
-            return (strs[0])
-
-        pref = strs[0]
-
-        for i in range(len(pref)):
-            for s in strs:
-                if i == len(s) or s[i] != strs[0][i]:
-                    return res
-            res += strs[0][i]
+    if len(strs) == 0:
         return res
+    if len(strs) == 1:
+        return (strs[0])
+
+    pref = strs[0]
+
+    for i in range(len(pref)):
+        for s in strs:
+            if i == len(s) or s[i] != strs[0][i]:
+                return res
+        res += strs[0][i]
+    return str(res)
--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # {} dict
 
 from Roman import romanToInt
+from CommonPrefix import longestCommonPrefix
 '''
 def main():
     username = input("Enter username:")
@@ -17,6 +18,17 @@ def main():
     modified_data = romanToInt(romanNumber)
     print(modified_data)
 
+def commonPrefix():
+    input_string = input('Enter elements of a list separated by space ')
+    print("\n")
+    user_list = input_string.split()
+    # print list
+    print('list: ', user_list)
+    modified_data = longestCommonPrefix(user_list)
+    print(modified_data)
+
+
 
 if __name__ == "__main__":
-    main();
+    main()
+    commonPrefix()
