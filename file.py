63e1718a0b7540f979ebf3520d3d18ef265f1efd
sawantvidisha10 <sawantvidisha10@gmail.com>
2022-06-09 18:07:21 -0700

    updates

---
+++ file.py
@@ -0,0 +1,12 @@
+file = open("test.txt", "r")
+file1 = open("demo.txt","w")
+file1.write("abcd jhagsgs jahsgds6ejhgsb kajdhgshdbshgjh")
+b= open("demo.txt","r")
+print(b)
+
+a = file.read()
+print(a)
+file.close()
+file1.close()
+
+
---
+++ strstr.py
@@ -0,0 +1,7 @@
+def strStr(self, haystack: str, needle: str) -> int:
+    if needle in haystack:
+        return haystack.index(needle)
+    elif needle == "":
+        return 0
+    else:
+        return -1
