64167b3381a1607e0b9ffcf91fe5af0a32b1133e
sawantvidisha10 <sawantvidisha10@gmail.com>
2022-06-24 01:54:35 -0700

    updates

--- List.py
+++ List.py
@@ -1,3 +1,5 @@
+import random
+
 fav_movies =["Sandlot", "The Lego Movie","Dune"]
 print(len(fav_movies))
 
@@ -13,3 +15,25 @@ print(fav_movies)
 while len(fav_movies) > 1:
     del (fav_movies[-1])
 print(fav_movies)
+
+#loop 40 times and print the number of the loop times 2. ex 2,4,6,8
+
+for number in range(40):
+    print((number+1)*2)
+
+cats = {"Jane": 14, "Cat": 5, "Tom": 8}
+
+cats["Wilson"] = 1 # to add to the dict
+print(cats)
+del(cats["Tom"]) #to delete specific keyvalue pair
+
+print (cats)
+print(len(cats))
+
+students = {1:True, 2: False}
+val = random.randint(1,2)
+
+if val == 1:
+    print(students[1])
+elif val ==2 :
+    print(students[2])
